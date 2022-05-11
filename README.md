cfd8b9242cdd40d7c3cdba4ce930e72781ad2c7d
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-11 13:12:46 +0800

    feat: mvp

--- README.md
+++ README.md
@@ -2,6 +2,31 @@
 
 <a href="https://marketplace.visualstudio.com/items?itemName=antfu.vscode-goto-alias" target="__blank"><img src="https://img.shields.io/visual-studio-marketplace/v/antfu.vscode-goto-alias.svg?color=eee&amp;label=VS%20Code%20Marketplace&logo=visual-studio-code" alt="Visual Studio Marketplace Version" /></a>
 
+Goto the source definition for aliased types.
+
+## Motivation
+
+For example, in [Nuxt 3](https://github.com/nuxt/framework) or [Vitesse](https://github.com/antfu/vitesse) projects, we provide auto import for APIs and components. To provide types for these auto imports, the tools will generate a `.d.ts` file to declare those APIs as "global" type.
+
+```ts
+// generated.d.ts
+declare global {
+  const autoImported: typeof import('foobar')['autoImported'] // alias to provide type from package 'foobar'
+  // ...
+}
+```
+
+With this declaration file, we could have type safety using `autoImported` in our code without importing it.
+
+```ts
+// no need to import
+const foo = autoImported()
+```
+
+The only small downside is that when you use "Go to definition" in the code, the IDE will redirect you the the definition file we generated instead of the real definition source.
+
+So, this extension is built to solve this. With this extension installed, when the "Go to definition" command hits the definition file, it will then redirect again to the definition source.
+
 ## Sponsors
 
 <p align="center">
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,80 @@
-import { window } from 'vscode'
+import type { TextDocument } from 'vscode'
+import { Position, Selection, commands, languages, window } from 'vscode'
+
+// TODO: config
+const CLOSE_DTS_TAB = false
 
 export function activate() {
-  window.showInformationMessage('Hello')
+  let triggerDoc: TextDocument | undefined
+  let lastDoc: TextDocument | undefined
+
+  languages.registerDefinitionProvider([
+    'javascript',
+    'typescript',
+    'javascriptreact',
+    'typescriptreact',
+    'vue',
+  ], {
+    provideDefinition(document: TextDocument) {
+      // record the last document triggers the "goto definition"
+      triggerDoc = document
+      return null
+    },
+  })
+
+  window.onDidChangeActiveTextEditor(async (e) => {
+    if (!e || !e.document)
+      return
+
+    const fn = async () => {
+      if (!e.selection.isSingleLine)
+        return
+
+      // to check if this editor change is triggered by "goto definition"
+      if (!triggerDoc || triggerDoc !== lastDoc)
+        return
+
+      // we only handles redirection to dts file
+      const path = e.document.uri.fsPath
+      if (!path.endsWith('.d.ts'))
+        return
+
+      // wait for e.selection to update (init 0)
+      await Promise.resolve()
+
+      const line = e.document.lineAt(e.selection.anchor.line)
+      const text = line.text
+      const regex = /:\s+typeof import\(.*?\)/
+      const match = text.match(regex)
+      if (!match)
+        return
+
+      // select the import name and trigger "goto definition" again
+      e.selection = new Selection(
+        new Position(
+          e.selection.anchor.line,
+          match.index! + match[0].length,
+        ),
+        new Position(
+          e.selection.anchor.line,
+          match.index! + match[0].length + 1,
+        ),
+      )
+      const tab = window.tabGroups.activeTabGroup.activeTab
+      triggerDoc = undefined
+      await commands.executeCommand('editor.action.goToDeclaration')
+      if (CLOSE_DTS_TAB && tab && tab !== window.tabGroups.activeTabGroup.activeTab)
+        await window.tabGroups.close(tab)
+    }
+
+    await fn()
+    await Promise.resolve()
+    lastDoc = window.activeTextEditor?.document
+  })
+
+  setTimeout(() => {
+    lastDoc = window.activeTextEditor?.document
+  }, 100)
 }
 
 export function deactivate() {
