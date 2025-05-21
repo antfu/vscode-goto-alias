ccae8de7872477b0272fc1704617752926d65a5a
Vida Xie <38204901+9romise@users.noreply.github.com>
2025-05-21 21:12:43 +0800

    feat!: drop cjs, require VSCode `1.100`

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,11 @@
 import process from 'node:process'
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: [
     'src/index.ts',
   ],
-  format: ['cjs'],
+  format: 'esm',
   shims: false,
   dts: false,
   sourcemap: process.env.NODE_ENV === 'development',
